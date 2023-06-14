1a3da4596a8f5bb6fc2fb67fa79f90713271de45
SarathAdhi <s.sarathadhithya@gmail.com>
2023-06-14 13:27:43 +0530

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,18 +5,6 @@ import vercel from "@astrojs/vercel/serverless";
 import compress from "astro-compress";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
-import tsConfig from "./tsconfig.json";
-import path, { resolve } from "path";
-
-const alias = tsConfig.compilerOptions.paths;
-const dirname = resolve();
-
-const resolvedAliases = Object.fromEntries(
-  Object.entries(alias).map(([key, value]) => [
-    key.replaceAll("/*", ""),
-    resolve(dirname, value[0].replaceAll("/*", "")),
-  ])
-);
 
 // https://astro.build/config
 export default defineConfig({
@@ -38,21 +26,4 @@ export default defineConfig({
     }),
     compress(),
   ],
-  vite: {
-    build: {
-      rollupOptions: {
-        external: [
-          "react", // ignore react stuff
-          "react-dom",
-        ],
-      },
-    },
-    resolve: {
-      alias: {
-        ...resolvedAliases,
-        "./runtimeConfig": "./runtimeConfig.browser",
-        "jss-plugin-{}": "jss-plugin-global",
-      },
-    },
-  },
 });
